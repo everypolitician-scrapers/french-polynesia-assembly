0c5c100ffec4bf1a38586c23b9e22c598b724f6c
Tony Bowden <tony@tmtm.com>
2015-08-15 15:30:55 +0100

    initial scraper

---
+++ scraper.rb
@@ -0,0 +1,62 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
+
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+  # Nokogiri::HTML(open(url).read, nil, 'utf-8')
+end
+
+def scrape_url(url, gender)
+  noko = noko_for(url)
+  noko.css('#deuxlistes a[href*="vos-representants"]/@href').each do |link|
+    scrape_person(link.text, gender)
+  end
+end
+
+def scrape_person(url, gender)
+  noko = noko_for(url)
+  box = noko.css('div.item-page')
+
+  data = { 
+    id: url[/rep=(.*)/, 1],
+    name: box.css('div[style*="font-size:22px"]').text.tidy,
+    area: box.xpath('.//text()[contains(.,"Section :")]/../span').text,
+    party: box.xpath('.//text()[contains(.,"Political group :")]/../following-sibling::div[1]').text.strip,
+    email: box.xpath('.//text()[contains(.,"Contact :")]/../following-sibling::div/a').text,
+    image: box.css('a[rel="sexylightbox"]/@href').text,
+    gender: gender,
+    term: '2013',
+    source: url,
+  }
+  data[:image] = URI.join(url, URI.escape(data[:image])).to_s unless data[:image].to_s.empty?
+  ScraperWiki.save_sqlite([:id, :term], data)
+end
+
+term = { 
+  id: '2013',
+  name: '2013–',
+  start_date: '2013-05-17',
+  source: 'https://fr.wikipedia.org/wiki/Assemblée_de_la_Polynésie_française',
+}
+ScraperWiki.save_sqlite([:id], term, 'terms')
+
+['m', 'f'].each do |sexe|
+  url = "http://www.assemblee.pf/_contenu/_pages_jumi/__recherche-rep-en.php?req=SELECT%20representant.actif,%20representant.id_representant,%20representant.nom%20AS%20nom_rep,%20representant.fichier_photo,%20representant.prenom%20AS%20prenom_rep,%20groupe_politique.nom%20AS%20nom_groupe,%20section.titre%20AS%20titre_section,%20groupe_politique.code_couleur%20FROM%20%20representant,%20groupe_politique,%20section%20WHERE%20actif=1%20AND%20representant.nom!=%27VACANT%27%20AND%20groupe_politique_id=id_groupe_politique%20AND%20section_id=id_section%20AND%20sexe=%27#{sexe}%27%20ORDER%20BY%20nom_rep"
+  scrape_url(url, sexe)
+end
+
