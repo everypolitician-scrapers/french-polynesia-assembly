08ad0f194aead195c2a482f193516f551cbdb7b8
Tony Bowden <tony@tmtm.com>
2017-04-30 12:05:16 +0100

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -11,13 +12,13 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
 def noko_for(url)
   Nokogiri::HTML(open(url).read)
-  # Nokogiri::HTML(open(url).read, nil, 'utf-8')
+  #  Nokogiri::HTML(open(url).read, nil, 'utf-8')
 end
 
 def scrape_url(url, gender)
@@ -31,28 +32,27 @@ def scrape_person(url, gender)
   noko = noko_for(url)
   box = noko.css('div.item-page')
 
-  data = { 
-    id: url[/rep=(.*)/, 1],
-    name: box.css('div[style*="font-size:22px"]').text.sub(/\s+\([^)]+\)/,'').tidy,
-    area: box.xpath('.//text()[contains(.,"Section :")]/../span').text,
-    party: box.xpath('.//text()[contains(.,"Political group :")]/../following-sibling::div[1]').text.strip,
-    email: box.xpath('.//text()[contains(.,"Contact :")]/../following-sibling::div/a').text,
-    image: box.css('a[rel="sexylightbox"]/@href').text,
+  data = {
+    id:     url[/rep=(.*)/, 1],
+    name:   box.css('div[style*="font-size:22px"]').text.sub(/\s+\([^)]+\)/, '').tidy,
+    area:   box.xpath('.//text()[contains(.,"Section :")]/../span').text,
+    party:  box.xpath('.//text()[contains(.,"Political group :")]/../following-sibling::div[1]').text.strip,
+    email:  box.xpath('.//text()[contains(.,"Contact :")]/../following-sibling::div/a').text,
+    image:  box.css('a[rel="sexylightbox"]/@href').text,
     gender: gender,
-    term: '2013',
+    term:   '2013',
     source: url,
   }
   data[:image] = URI.join(url, URI.escape(data[:image])).to_s unless data[:image].to_s.empty?
-  ScraperWiki.save_sqlite([:id, :term], data)
+  ScraperWiki.save_sqlite(%i[id term], data)
 end
 
-gender_map = { 
+gender_map = {
   'm' => 'male',
-  'f' => 'female'
+  'f' => 'female',
 }
 
-['m', 'f'].each do |sexe|
+%w[m f].each do |sexe|
   url = "http://www.assemblee.pf/_contenu/_pages_jumi/__recherche-rep-en.php?req=SELECT%20representant.actif,%20representant.id_representant,%20representant.nom%20AS%20nom_rep,%20representant.fichier_photo,%20representant.prenom%20AS%20prenom_rep,%20groupe_politique.nom%20AS%20nom_groupe,%20section.titre%20AS%20titre_section,%20groupe_politique.code_couleur%20FROM%20%20representant,%20groupe_politique,%20section%20WHERE%20actif=1%20AND%20representant.nom!=%27VACANT%27%20AND%20groupe_politique_id=id_groupe_politique%20AND%20section_id=id_section%20AND%20sexe=%27#{sexe}%27%20ORDER%20BY%20nom_rep"
   scrape_url(url, gender_map[sexe])
 end
-
